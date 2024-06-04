fff99226522d2c1a2eafe973111413219e4e56ed
Ibrahima <indiaye@lithesolutions.com>
2024-06-04 19:05:01 -0400

    add webpack, babel

---
+++ web/modules/custom/react_integration/src/Controller/ReactIntegrationController.php
@@ -0,0 +1,38 @@
+<?php
+namespace Drupal\react_integration\Controller;
+use Drupal\Core\Controller\ControllerBase ;
+
+class ReactIntegrationController extends ControllerBase {
+
+  public function content () {
+
+   return [
+    '#markup' => '<div> id="react-root"</div>',
+
+ ];
+  }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ web/modules/custom/react_integration/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return <div>Hello, React in Drupal!</div>;
+};
+ReactDOM.render(<App />, document.getElementById('react-root'));
